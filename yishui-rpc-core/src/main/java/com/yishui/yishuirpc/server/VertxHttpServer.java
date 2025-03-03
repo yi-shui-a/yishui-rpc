ec3416be38949fa2a5b27e838c85c4f02ddbd558
yishui <1480685741@qq.com>
2025-03-04 00:32:07 +0800

    1.2 添加请求回复封装代码

--- yishui-rpc-core/src/main/java/com/yishui/yishuirpc/server/VertxHttpServer.java
+++ yishui-rpc-core/src/main/java/com/yishui/yishuirpc/server/VertxHttpServer.java
@@ -16,15 +16,16 @@ public class VertxHttpServer implements HttpServer {
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         // 监听端口并处理请求
-        server.requestHandler(request -> {
-            // 处理 HTTP 请求
-            System.out.println("Received request: " + request.method() + " " + request.uri());
-
-            // 发送 HTTP 响应
-            request.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP server!");
-        });
+        server.requestHandler(new HttpServerHandler());
+//        server.requestHandler(request -> {
+//            // 处理 HTTP 请求
+//            System.out.println("Received request: " + request.method() + " " + request.uri());
+//
+//            // 发送 HTTP 响应
+//            request.response()
+//                    .putHeader("content-type", "text/plain")
+//                    .end("Hello from Vert.x HTTP server!");
+//        });
 
         // 启动 HTTP 服务器并监听指定端口
         server.listen(port, result -> {
